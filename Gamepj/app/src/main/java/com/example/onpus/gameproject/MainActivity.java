4533e64aec14873e06e14443ef73d624e50a57a9
DESKTOP-MBO6HON\onpus <onpusnow@gmail.com>
2017-10-13 20:35:10 +0800

    Revert "start page"
    This reverts commit 34694099749e5a72e4507f57c4bba4461ad99c06.

--- Gamepj/app/src/main/java/com/example/onpus/gameproject/MainActivity.java
+++ Gamepj/app/src/main/java/com/example/onpus/gameproject/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.onpus.gameproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
