81a8fddc60159f283a648a190c7a2022c0f7b881
SimSim <honyuetsim@gmail.com>
2017-10-14 16:09:13 +0800

    add buttons and menu

---
+++ Gamepj/app/src/main/java/com/example/onpus/gameproject/Game.java
@@ -0,0 +1,87 @@
+package com.example.onpus.gameproject;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuAbout:
+                new AlertDialog.Builder(this)
+                        .setTitle("About")
+                        .setMessage("About content")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).show();
+                break;
+            case R.id.restart:
+                new AlertDialog.Builder(this)
+                        .setTitle("Restart")
+                        .setMessage("Are you sure to restart the game?")
+                        .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //code for restart the game
+                            }
+                        }).show();
+                break;
+            case R.id.endGame:
+                AlertDialog.Builder scoreAlert = new AlertDialog.Builder(this);
+                scoreAlert.setTitle("Score")
+                        .setMessage("Your Score is"+ " Score")
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(
+                                            DialogInterface dialog, int id) {
+                                        finish();
+                                    }
+                                });
+                // create alert dialog
+                final AlertDialog scoreAlertDialog = scoreAlert.create();
+
+                new AlertDialog.Builder(this)
+                        .setTitle("End the Game")
+                        .setMessage("Are you sure to end the game?")
+                        .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                scoreAlertDialog.show();
+                            }
+                        }).show();
+                break;
+        }
+        return true;
+    }
+}
--- Gamepj/app/src/main/java/com/example/onpus/gameproject/MainActivity.java
+++ Gamepj/app/src/main/java/com/example/onpus/gameproject/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.onpus.gameproject;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = (Button) findViewById(R.id.start);
+        Button about = (Button) findViewById(R.id.about);
+        Button highScore = (Button) findViewById(R.id.highScore);
+        Button exit = (Button) findViewById(R.id.exit);
+
+        start.setOnClickListener(this);
+        about.setOnClickListener(this);
+        highScore.setOnClickListener(this);
+        exit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.start:
+                Intent intent = new Intent(this, Game.class);
+                startActivity(intent);
+                break;
+            case R.id.about:
+                new AlertDialog.Builder(this)
+                        .setTitle("About")
+                        .setMessage("About content")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).show();
+                break;
+            case R.id.highScore:
+                new AlertDialog.Builder(this)
+                        .setTitle("High Score")
+                        .setMessage("Your High Score is "+""+"!")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).show();
+                break;
+            case R.id.exit:
+                new AlertDialog.Builder(this)
+                        .setTitle("Exit")
+                        .setMessage("Sure Exit?")
+                        .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("Stay", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).show();
+                break;
+            default:
+        }
+
     }
 }
